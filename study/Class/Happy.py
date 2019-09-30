09fb6f605b3a60c939cca171b4c1de6fd1981a88
kkplant <xiongchen_1988@hotmail.com>
2019-09-30 10:09:49 +0800

    Create Happy.py

---
+++ study/Class/Happy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+#class counter(object):
+    #def 
+
+class Happy(object):
+    def __init__(self, Num):
+        self._Num = -int(Num)
+    def pp(self, num):
+        print(num)
+
+if __name__ == '__main__':
+    a = Happy(9)
+    #a.pp(2)
+    print(a)
