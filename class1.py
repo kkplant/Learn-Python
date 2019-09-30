72e4988c4b8d52e46305cb8d7beacc15c38357ce
kkplant <xiongchen_1988@hotmail.com>
2019-09-30 09:45:21 +0800

    Create class1.py

---
+++ class1.py
@@ -0,0 +1,20 @@
+class FooClass(object):
+"""my very first class:FooClass"""
+version = 0.1 
+#class (data) attribute
+def __init__(self, nm = 'John Doe'):
+"""construtor"""
+self.name = nm
+#class instance (data) attribute
+print('Created a class instance for',nm)
+def showname(self):
+	display instance attribute and class name
+print('Your name is',self.name)
+print('My name is',self.__class__.name)
+def showver(self):
+"""display class(static) attribute"""
+print(self.version)
+#references FooClass.version
+def addMe2Me(self,x):#does not use 'self'
+"""apply + operation to argument"""
+return x + x 
